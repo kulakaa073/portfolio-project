a4a34098657dbbbd1899ddc5e23a2f86afbee903
Shade <kulakaa073@gmail.com>
2025-04-02 21:41:15 +0300

    more JS
    progress save

--- src/main.js
+++ src/main.js
@@ -1,54 +1,87 @@
-import Accordion from "accordion-js";
-import Swiper from "swiper";
+import Accordion from 'accordion-js';
+import Swiper from 'swiper';
+import { Navigation, Keyboard } from 'swiper/modules';
+import 'swiper/css';
 
-const burgerMenuButton = document.querySelector(".burger-menu");
-const mobileMenu = document.querySelector(".mobile-menu-overlay");
-const navMenuButton = document.querySelector(".nav-menu-button");
-const navMenu = document.querySelector(".nav-list");
-const navBlock = document.querySelector(".navigation"); 
-const aboutInfoBlock = document.querySelector(".about-info");
+const burgerMenuButton = document.querySelector('.burger-menu');
+const mobileMenu = document.querySelector('.mobile-menu-overlay');
+const navMenuButton = document.querySelector('.nav-menu-button');
+const navMenu = document.querySelector('.nav-list');
+const navBlock = document.querySelector('.navigation');
+const aboutInfoBlock = document.querySelector('.about-info');
+const aboutSwiperBlock = document.querySelector('.about-swiper');
+const projectsSwiperBlock = document.querySelector('.projects-swiper');
+const faqBlock = document.querySelector('.faq-questions-list');
 
-const accordionOptions = {
-    elementClass: "about-item",
-    triggerClass: "about-info-expand-button",
-    panelClass: "about-panel-wrap",
-    onOpen: (currElement) => {currElement.querySelector()},
-    onClose: (currElement) => {}
-}
-new Accordion(".about-info", { ...accordionOptions });
+const aboutAccordionOptions = {
+  elementClass: 'about-item',
+  triggerClass: 'about-info-expand-button',
+  panelClass: 'about-panel-wrap',
+};
 
-const swiperOptions = {
-    wrapperClass: "about-tech-stack",
-    navigation: {
-        nextEl: '.about-navigation-button-next',
-        prevEl: '.about-navigation-button-prev',
+new Accordion(aboutInfoBlock, { ...aboutAccordionOptions });
+
+const faqAccordionOptions = {
+  elementClass: 'faq-question-card',
+  triggerClass: 'faq-expand-button',
+  panelClass: 'faq-answer',
+};
+
+new Accordion(faqBlock, { ...faqAccordionOptions });
+
+const aboutSwiperOptions = {
+  modules: [Navigation, Keyboard],
+  loop: true,
+  navigation: {
+    nextEl: '.about-swiper-button-next',
   },
-}
-const swiper = new Swiper('.swiper', {...swiperOptions});
+  slidesPerView: 2,
+  wrapperClass: 'tech-stack',
+  slideClass: 'tech-name',
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+};
 
-burgerMenuButton.addEventListener("click", (e) => {
-    mobileMenu.style.display = 'block';
-    lockScroll();
-})
+new Swiper(aboutSwiperBlock, { ...aboutSwiperOptions });
 
-mobileMenu.addEventListener("click", (e) => {
-    if (e.target.nodeName === "A" || e.target.nodeName === "BUTTON") {
-        mobileMenu.style.display = 'none';
-        unlockScroll();
-    }
-})
+const projectSwiperOptions = {
+  modules: [Navigation, Keyboard],
+  navigation: {
+    nextEl: '.project-swiper-controlls-next',
+    prevEl: '.project-swiper-controlls-prev',
+  },
+  wrapperClass: 'projects-list',
+  slideClass: 'project-card',
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+};
 
-navBlock.addEventListener("click", (e) => {
-    console.log(navMenu.classList.contains("visually-hidden"));
-    if (e.target === navMenuButton) {
-        if (navMenu.classList.contains("visually-hidden"))
-            navMenu.classList.remove("visually-hidden");
-        else 
-            navMenu.classList.add("visually-hidden");
-    }
-    else 
-        if (!navMenu.classList.contains("visually-hidden"))
-            navMenu.classList.add("visually-hidden");
+new Swiper(projectsSwiperBlock, { ...projectSwiperOptions });
+
+burgerMenuButton.addEventListener('click', e => {
+  mobileMenu.style.display = 'block';
+  lockScroll();
+});
+
+mobileMenu.addEventListener('click', e => {
+  if (e.target.nodeName === 'A' || e.target.nodeName === 'BUTTON') {
+    mobileMenu.style.display = 'none';
+    unlockScroll();
+  }
+});
+
+navBlock.addEventListener('click', e => {
+  console.log(navMenu.classList.contains('visually-hidden'));
+  if (e.target === navMenuButton) {
+    if (navMenu.classList.contains('visually-hidden'))
+      navMenu.classList.remove('visually-hidden');
+    else navMenu.classList.add('visually-hidden');
+  } else if (!navMenu.classList.contains('visually-hidden'))
+    navMenu.classList.add('visually-hidden');
 });
 
 //#region scroll lock stuff
@@ -56,13 +89,13 @@ navBlock.addEventListener("click", (e) => {
 function lockScroll() {
   // Store the current scroll position
   const scrollY = window.scrollY;
-  
+
   // Add styles to the body
   document.body.style.position = 'fixed';
   document.body.style.top = `-${scrollY}px`;
   document.body.style.width = '100%';
   document.body.style.overflowY = 'scroll';
-  
+
   // Store the scroll position as a data attribute
   document.body.dataset.scrollY = scrollY;
 }
@@ -71,13 +104,13 @@ function lockScroll() {
 function unlockScroll() {
   // Get the stored scroll position
   const scrollY = document.body.dataset.scrollY;
-  
+
   // Remove the fixed positioning
   document.body.style.position = '';
   document.body.style.top = '';
   document.body.style.width = '';
   document.body.style.overflowY = '';
-  
+
   // Scroll back to the original position
   window.scrollTo(0, parseInt(scrollY || '0'));
 }
